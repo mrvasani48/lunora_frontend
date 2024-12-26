05e679c75dc2c07e238c7f0f9a3c4718c4eb55c4
mrvasani48 <vasanikishan48@gmail.com>
2024-12-26 17:04:00 +0530

    2024-12-12 :: socket

--- app/chat/SocketContext.tsx
+++ app/chat/SocketContext.tsx
@@ -1,32 +1,32 @@
-'use client';
-import { createContext, useContext, useEffect, useState } from 'react';
-import io from 'socket.io-client';
+// 'use client';
+// import { createContext, useContext, useEffect, useState } from 'react';
+// import io from 'socket.io-client';
 
-// Create the context for Socket
-const SocketContext = createContext(null);
+// // Create the context for Socket
+// const SocketContext = createContext(null);
 
-export const useSocket = () => useContext(SocketContext);
+// export const useSocket = () => useContext(SocketContext);
 
-export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const [socket, setSocket] = useState(null);
+// export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+//   const [socket, setSocket] = useState(null);
 
-  useEffect(() => {
-    // Connect to the WebSocket server running on port 8000
-    const socketInstance = io('http://localhost:8000', {
-      transports: ['websocket', 'polling'], // Enable both transports (WebSocket and Polling)
-    });
+//   useEffect(() => {
+//     // Connect to the WebSocket server running on port 8000
+//     const socketInstance = io('http://localhost:8000', {
+//       transports: ['websocket', 'polling'], // Enable both transports (WebSocket and Polling)
+//     });
 
-    socketInstance.on('connect_error', (error) => {
-      console.error("Socket connection error:", error);
-    });
+//     socketInstance.on('connect_error', (error) => {
+//       console.error("Socket connection error:", error);
+//     });
 
-    setSocket(socketInstance);
+//     setSocket(socketInstance);
 
-    return () => {
-      socketInstance.disconnect();
-    };
-  }, []);
+//     return () => {
+//       socketInstance.disconnect();
+//     };
+//   }, []);
 
 
-  return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
-};
+//   return <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>;
+// };
